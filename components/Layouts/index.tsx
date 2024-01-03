d42babd8981d27120b6a340972b80cf295baa563
Naveen Bandarage <thebanterage@gmail.com>
2024-01-04 12:18:36 +1300

    added speedinsights to page

--- components/Layouts/index.tsx
+++ components/Layouts/index.tsx
@@ -1,14 +1,14 @@
 import Footer from "../Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function Main({ children }) {
   return (
     <>
       <main className="m:px-0 flex justify-center px-6 pt-8 sm:pt-32">
-        <article className="w-full max-w-main grow">
-          {children}
-        </article>
+        <article className="w-full max-w-main grow">{children}</article>
       </main>
       <Footer />
+      <SpeedInsights />
     </>
   );
 }
