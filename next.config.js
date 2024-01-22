67455531db97530fd342a6e1310f028b457f1491
Restyled.io <commits@restyled.io>
2024-01-22 03:18:25 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,34 +1,34 @@
 const withMDX = require("@next/mdx")({
-  extension : /\.mdx?$/,
+  extension: /\.mdx?$/,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode : true,
-  swcMinify : true,
-  images : {
-    formats : [ "image/avif", "image/webp" ],
-    domains : [ "images.ctfassets.net", "i.scdn.co" ],
-    remotePatterns : [
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: ["images.ctfassets.net", "i.scdn.co"],
+    remotePatterns: [
       {
-        protocol : "https",
-        hostname : "**.digitaloceanspaces.com",
+        protocol: "https",
+        hostname: "**.digitaloceanspaces.com",
       },
       {
-        protocol : "http",
-        hostname : "books.google.com",
+        protocol: "http",
+        hostname: "books.google.com",
       },
       {
-        protocol : "https",
-        hostname : "assets.literal.club",
+        protocol: "https",
+        hostname: "assets.literal.club",
       },
       {
-        protocol : "https",
-        hostname : "**.spotifycdn.com",
+        protocol: "https",
+        hostname: "**.spotifycdn.com",
       },
     ],
   },
-  webpack : (config, {webpack}) => {
+  webpack: (config, { webpack }) => {
     /* Hide error "Critical dependency: the request of a dependency is an
      * expression" from remark-textr */
     config.plugins.push(new webpack.ContextReplacementPlugin(/remark-textr/));
@@ -38,7 +38,7 @@ const nextConfig = {
 };
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled : process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withMDX(nextConfig);
