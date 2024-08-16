8119ffe637fa3db870e0a9f0a425ee86ff1c5dbd
Naveen Bandarage <thebanterage@gmail.com>
2024-08-16 15:40:50 +1000

    initial commit and reformating

--- components/Layouts/index.tsx
+++ components/Layouts/index.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 export function Main({ children }) {
   return (
     <>
-      <main className="m:px-0 flex justify-center px-6 pt-8 sm:pt-32">
+      <main className="m:px-0 flex justify-center px-6 pt-6 sm:pt-12">
         <article className="w-full max-w-main grow">{children}</article>
       </main>
       <Footer />
